116833c7ba49936ae2429da4889e5c59c5f1e1a7
SeriyTk <gomenuk.serg@gmail.com>
2021-12-14 22:34:21 +0200

    14.12.21

---
+++ Переменные.py
@@ -0,0 +1,29 @@
+number = 3
+number = 6
+number1 = 5
+res = number + number1
+print(res)
+
+num1=num2=6
+print(num1,num2)
+
+num_1,num_2 = 1,2
+print(num_1,num_2)
+
+swap1=8
+swap2=9
+swap1, swap2 = swap2, swap1
+print(swap1, swap2)
+
+swap2 -= number
+print(swap2)
+
+a,b,c=[1,2,3]
+print(a)
+print(b)
+print(c)
+print()
+a,b,*c=[1,2,3,4,5]
+print(a)
+print(b)
+print(c)
