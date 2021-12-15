95c753207d814d72ea06327ef0a9dcfe6f2915c3
SeriyTk <gomenuk.serg@gmail.com>
2021-12-15 21:14:48 +0200

    15.12.21

---
+++ Типы данных.py
@@ -0,0 +1,20 @@
+a = None
+print(type(a))
+a = 1
+print(type(a))
+a = 1.0
+print(type(a))
+a = 1 + 1j
+print(type(a))
+a = '1'
+print(type(a))
+a = [1,1,'a']
+print(type(a))
+a = (1,1,'a')
+print(type(a))
+a = {1,1,'a'}
+print(type(a))
+a = {'a':2,'b':1}
+print(type(a))
+a = True
+print(type(a))
